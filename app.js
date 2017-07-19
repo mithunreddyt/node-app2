882bd40947fdc63d625e489b3b2015b3df46c71b
mithun <mithunreddythunga@gmail.com>
2017-07-19 00:11:42 -0500

    original commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//grab EXPRESS
+
+var express = require('express');
+
+//create an express app
+
+var app = express();
+
+//create an express route fot the home page
+//http://localhost:8080/
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+//start the server on port 8080
+app.listen(8080);
+
+//send a message
+console.log('server has started');
